59739b8ee094644ce6f41ce0fe1793b4b8bf726e
JocmanHUN <135754827+JocmanHUN@users.noreply.github.com>
2023-10-06 13:09:41 +0200

    Content.php javítás

--- Alkalmazas(ok)/miniprojekt/content.php
+++ Alkalmazas(ok)/miniprojekt/content.php
@@ -1,15 +1,14 @@
 <?php
-    if(isset($_GET['page']) && !empty($_GET['page'])) {
-        switch($_GET['page']) {
-            case 1:
-                require_once('login.php');
-                break;
-            case 2:
-                require_once('register.php');
-                break;
+if(isset($_GET['page']) && !empty($_GET['page'])) {
+    switch($_GET['page']) {
+        case 1:
+            require_once('login.php');
+            break;
+        case 2:
+            require_once('register.php');
+            break;
     }
-    else {
-        header("Location: index.php?page=1");
-    }
+} else {
+    header("Location: login.php");
 }
-?> 
+?>
